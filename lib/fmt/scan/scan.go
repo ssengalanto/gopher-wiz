b18a20f082a35e04a68128d660f0deea38d43f30
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-04-06 12:23:11 +0800

    lib(fmt): scan

---
+++ lib/fmt/scan/scan.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var (
+		fname string
+		lname string
+		age   int
+	)
+
+	fmt.Print("Enter your first and last name: ")
+	n, err := fmt.Scan(&fname, &lname)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Print("Enter your age: ")
+	m, err := fmt.Scan(&age)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d items scanned. Hello %s %s!\n", n, fname, lname)
+	fmt.Printf("%d items scanned. %d years old!\n", m, age)
+}

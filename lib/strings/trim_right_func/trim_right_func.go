4e3622bcbffd3f3f010994dce9aaca047bc590a2
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-04-20 13:07:46 +0800

    lib(strings): trim right func

---
+++ lib/strings/trim_right_func/trim_right_func.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	f := func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	}
+
+	x := strings.TrimRightFunc("-0_Gopher Wiz_0-", f)
+	fmt.Println("x:", x)
+}

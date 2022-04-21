718cab496669fefb0d4d0c80fadd52fcc7ada7ba
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-04-21 11:07:03 +0800

    concurrency: channel ownership

---
+++ concurrency/channel_ownership/channel_ownership.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+	Channel owner is responsible for the ff:
+	1. Instantiate the channel
+	2. Perform writes, or pass ownership to another goroutine
+	3. Close the channel
+	4. Encapsulate the previous three things in this list and expose them
+	   via a read-only channel.
+*/
+
+func main() {
+	fn := func() <-chan int {
+		n := 3
+		c := make(chan int, n)
+		go func() {
+			defer close(c)
+			for i := 1; i <= n; i++ {
+				c <- i
+			}
+		}()
+
+		return c
+	}
+
+	stream := fn()
+	for v := range stream {
+		fmt.Printf("recieved: %d\n", v)
+	}
+
+	fmt.Println("Done receiving!")
+}

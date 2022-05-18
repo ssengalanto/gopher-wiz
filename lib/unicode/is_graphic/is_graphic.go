a024f32a52a0253b5c8dcd7a8f508b8ae82b85e5
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-05-18 10:34:21 +0800

    lib(unicode): is graphic

--- lib/unicode/is_graphic/is_graphic.go
+++ lib/unicode/is_graphic/is_graphic.go
@@ -5,16 +5,12 @@ import (
 	"unicode"
 )
 
-/*
-	key codes - https://bit.ly/3NilELT
-*/
-
 func main() {
 	// 0xd (13) is unicode code point of enter
-	x := unicode.IsControl(0xd)
+	x := unicode.IsGraphic(0xd)
 	fmt.Println("x:", x)
 
 	// 0x7e (126) is unicode point of '~'
-	y := unicode.IsControl(0x7e)
+	y := unicode.IsGraphic(0x7e)
 	fmt.Println("y", y)
 }

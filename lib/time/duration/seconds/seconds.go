7d5f4fd9f311e33b79e8622992533497b156de71
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-06-21 08:52:28 +0800

    lib(time): type duration - seconds

---
+++ lib/time/duration/seconds/seconds.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	h, m, s := t.Clock()
+	duration := fmt.Sprintf("%dh%dm%ds", h, m, s)
+
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+		panic(err)
+	}
+
+	x := d.Seconds()
+	fmt.Printf("x: %.2f secs", x)
+}

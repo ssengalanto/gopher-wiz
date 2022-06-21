1e3bdad339ddea33a109ea3dc9f2536cef01a68e
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-06-21 08:54:52 +0800

    lib(time): type duration - truncate

---
+++ lib/time/duration/truncate/truncate.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	h, m, s := t.Clock()
+	duration := fmt.Sprintf("%dh%dm%ds", h, m, s)
+
+	d, err := time.ParseDuration(duration)
+	if err != nil {
+		panic(err)
+	}
+
+	round := []time.Duration{
+		time.Nanosecond,
+		time.Microsecond,
+		time.Millisecond,
+		time.Second,
+		time.Minute,
+		time.Hour,
+	}
+
+	for _, r := range round {
+		fmt.Printf("(%6s) = %s\n", r, d.Truncate(r).String())
+	}
+}

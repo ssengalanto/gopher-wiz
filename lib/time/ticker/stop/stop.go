9e03d33a46dea51ead854c1cc773114ee39f06a9
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-07-04 13:32:44 +0800

    lib(time): type ticker - stop

---
+++ lib/time/ticker/stop/stop.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ticker := time.NewTicker(time.Second)
+
+	// stops the ticker when done is true
+	defer ticker.Stop()
+
+	done := make(chan bool)
+
+	go func() {
+		time.Sleep(10 * time.Second)
+		done <- true
+	}()
+
+	for {
+		select {
+		case <-done:
+			fmt.Println("Done!")
+			return
+		case t := <-ticker.C:
+			h, m, s := t.Clock()
+			fmt.Printf("Current time: %d:%d:%d\n", h, m, s)
+		}
+	}
+}
